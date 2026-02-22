25a99028ccc96fe76857651f60233ee493fd55c8
kujobark <nyt3kujo@yahoo.com>
2026-02-21 21:19:25 -0600

    Update tutor.js

--- api/tutor.js
+++ api/tutor.js
@@ -1,5 +1,5 @@
 import OpenAi from "openai";
-import { SAFETY_RESPONSES } from "../lib/safetyresponses.js";
+import { SAFETY_RESPONSES } from "../lib/safetyResponses.js";
 import { classifyMessage } from "../lib/safetyCheck.js";
 
 const client = new OpenAI({ apiKey: process.env.OPENAI_API_KEY });
